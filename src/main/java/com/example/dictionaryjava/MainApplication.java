1e2e23ba506a069b30e8654a9f712b58af6cedb3
mashumarodesu <phamthanhngan2012@gmail.com>
2021-10-23 09:58:20 +0700

    Random changes

--- src/main/java/com/example/dictionaryjava/MainApplication.java
+++ src/main/java/com/example/dictionaryjava/MainApplication.java
@@ -3,8 +3,6 @@ package com.example.dictionaryjava;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
